354305a168fba1048b3b2722361d41b870024c87
Глеб <gleb1.shiling@gmail.com>
2024-06-30 22:53:40 +0300

    В приложении candidates созданы вьюхи для реализации концепции CRUD: 1) Чтение (Read): 1.1) Просмотр общей информации о пользователе (customer_info) 1.2) Просмотр списка всего образования пользователя (education_list_info) 1.3) Просмотр списка всего опыта пользователя (experience_list_info) 1.4) Просмотра отдельного университета, в котором учился пользователь (education_info) 1.5) Просмотра отдельной практики, которая была у пользователя (experience_info)
    2) Создание (Create):
    2.1) Добавление пользователю нового университета (add_education)
    2.2) Добавление пользователю нового опыта работы (add_experience)
    P.s. создание нового пользователя уже было реализовано в вьюхе registration приложения system
    
    3) Обновление (Update):
    3.1) Обновление данных о пользователе (customer_update)
    3.2) Обновление данных об учёбе в конкретном университете (education_update)
    3.3) Обновление данных о конкретном опыте работы (experience_update)
    
    4) Удаление (Delete):
    4.1) Удаление пользователя (customer_delete)
    4.2) Удаление университета (education_delete)
    4.3) Удаление опыта (experience_delete)

--- backoffice/candidates/forms.py
+++ backoffice/candidates/forms.py
@@ -1,1 +1,12 @@
 from django import forms
+from .models import Education, Experience
+
+class EducationForm(forms.ModelForm):
+    class Meta:
+        model = Education
+        fields = ['university_name', 'faculty', 'spec_id', 'end_year']
+
+class ExperienceForm(forms.ModelForm):
+    class Meta:
+        model = Experience
+        fields = ['company', 'position', 'additional_info', 'start_date', 'end_date']
--- backoffice/candidates/views.py
+++ backoffice/candidates/views.py
@@ -1,1 +1,144 @@
 # тут должны быть все view функции связанные с резюме(CRUD, каталог, фильтр)
+
+from os import getenv
+
+from system.models import Customer, Grade, City, Spec
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
+from system.forms import CustomerForm
+
+from .models import Education, Experience
+from .forms import EducationForm, ExperienceForm
+
+@csrf_exempt
+def customer_info(request):
+    profile_id = request.GET.get('id')
+    customer = Customer.objects.filter(id=profile_id)
+    city = City.objects.filter(id=customer.city)
+    job_city = City.objects.filter(id=customer.job_city)
+    grade = Grade.objects.filter(id=customer.grade)
+    return JsonResponse(data={'first_name': customer.first_name, 'last_name': customer.last_name, 'email': customer.email, 'phone': customer.phone, 'gender': customer.gender,
+                              'telegram': customer.telegram, 'birthday': customer.birthday, 'city': city.name, 'job_city': job_city.name, 'grade': grade.name, 'role': customer.role}, status=200)
+
+@csrf_exempt
+def education_list_info(request):
+    user_id = request.GET.get('id')
+    education = Education.objects.all().filter(id=user_id)
+    return JsonResponse(data={'education': [{'university_name': university.university_name, 'faculty': university.faculty,
+                                             'spec': Spec.objects.filter(id=university.spec_id).name, 'end_year': university.end_year}] for university in education}, status=200)
+
+@csrf_exempt
+def experience_list_info(request):
+    user_id = request.GET.get('id')
+    all_experience = Experience.objects.all().filter(id=user_id)
+    return JsonResponse(data={'experience': [{'company': experience.company, 'position': experience.position, 'additional_info': experience.additional_info,
+                                              'start_date': experience.start_date, 'end_date': experience.end_date}] for experience in all_experience}, status=200)
+
+@csrf_exempt
+def education_info(request):
+    university_id = request.GET.get('id')
+    university = Education.objects.filter(id=university_id)
+    return JsonResponse(data={'university_name': university.university_name, 'faculty': university.faculty,
+                                             'spec': Spec.objects.filter(id=university.spec_id).name, 'end_year': university.end_year}, status=200)
+
+@csrf_exempt
+def experience_info(request):
+    experience_id = request.GET.get('id')
+    experience = Experience.objects.filter(id=experience_id)
+    return JsonResponse(data={'company': experience.company, 'position': experience.position, 'additional_info': experience.additional_info,
+                                              'start_date': experience.start_date, 'end_date': experience.end_date}, status=200)
+
+@csrf_exempt
+def add_education(request):
+    customer_id = request.GET.get('id')
+    if request.method == 'POST':
+        form = EducationForm(request.POST)
+        if request.headers['SECRET-SYSTEM'] != getenv('SECRET_SYSTEM'):
+            return HttpResponse({'status': False}, status=403)
+        if not form.is_valid():
+            return HttpResponse({'status': False}, status=400)
+        education = form.save()
+        education.user_id = customer_id
+        education.save()
+        return JsonResponse(data={'status': True, 'education': education.id}, status=200)
+
+@csrf_exempt
+def add_experience(request):
+    customer_id = request.GET.get('id')
+    if request.method == 'POST':
+        form = ExperienceForm(request.POST)
+        if request.headers['SECRET-SYSTEM'] != getenv('SECRET_SYSTEM'):
+            return HttpResponse({'status': False}, status=403)
+        if not form.is_valid():
+            return HttpResponse({'status': False}, status=400)
+        experience = form.save()
+        experience.user_id = customer_id
+        experience.save()
+        return JsonResponse(data={'status': True, 'experience': experience.id}, status=200)
+
+@csrf_exempt
+def customer_update(request):
+    customer = get_object_or_404(Customer, pk=request.GET.get('id'))
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, instance=customer)
+        if request.headers['SECRET-SYSTEM'] != getenv('SECRET_SYSTEM'):
+            return HttpResponse({'status': False}, status=403)
+        if not form.is_valid():
+            return HttpResponse({'status': False}, status=400)
+        email = form.cleaned_data['email']
+        customer = Customer.objects.filter(email=email)
+        if len(customer) > 0:
+            return HttpResponse({'status': False}, status=400)
+        else:
+            customer = form.save()
+            customer.set_password_hash(customer.password)
+            return JsonResponse(data={'status': True, 'user': customer.id}, status=200)
+    else:
+        return HttpResponse({'status': False}, status=405)
+
+@csrf_exempt
+def education_update(request):
+    education = get_object_or_404(Education, pk=request.GET.get('id'))
+    if request.method == 'POST':
+        form = EducationForm(request.POST, instance=education)
+        if request.headers['SECRET-SYSTEM'] != getenv('SECRET_SYSTEM'):
+            return HttpResponse({'status': False}, status=403)
+        if not form.is_valid():
+            return HttpResponse({'status': False}, status=400)
+        education = form.save()
+        return JsonResponse(data={'status': True, 'education': education.id}, status=200)
+    else:
+        return HttpResponse({'status': False}, status=405)
+
+@csrf_exempt
+def experience_update(request):
+    experience = get_object_or_404(Experience, pk=request.GET.get('id'))
+    if request.method == 'POST':
+        form = ExperienceForm(request.POST, instance=experience)
+        if request.headers['SECRET-SYSTEM'] != getenv('SECRET_SYSTEM'):
+            return HttpResponse({'status': False}, status=403)
+        if not form.is_valid():
+            return HttpResponse({'status': False}, status=400)
+        experience = form.save()
+        return JsonResponse(data={'status': True, 'experience': experience.id}, status=200)
+    else:
+        return HttpResponse({'status': False}, status=405)
+
+@csrf_exempt
+def customer_delete(request):
+    customer = get_object_or_404(Customer, pk=request.GET.get('id'))
+    customer.delete()
+    return JsonResponse(data={'status': True}, status=200)
+
+@csrf_exempt
+def education_delete(request):
+    education = get_object_or_404(Education, pk=request.GET.get('id'))
+    education.delete()
+    return JsonResponse(data={'status': True}, status=200)
+
+@csrf_exempt
+def experience_delete(request):
+    experience = get_object_or_404(Experience, pk=request.GET.get('id'))
+    experience.delete()
+    return JsonResponse(data={'status': True}, status=200)
